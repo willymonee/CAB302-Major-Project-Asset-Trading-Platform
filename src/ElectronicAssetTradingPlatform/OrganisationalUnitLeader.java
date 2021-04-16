5b1c2b85e84e633cfa0417f2ef3487d3e31f68bb
William <n10494448@qut.edu.au>
2021-04-16 21:37:31 +1000

    Refactored OrganisationUnitLeader class name --> OrganisationalUnitLeader

---
+++ src/ElectronicAssetTradingPlatform/OrganisationalUnitLeader.java
@@ -0,0 +1,11 @@
+package ElectronicAssetTradingPlatform;
+
+/**
+ * Class for the organisational unit leaders
+ */
+public class OrganisationalUnitLeader extends OrganisationalUnitMembers{
+    public OrganisationalUnitLeader(String username, String password, int organisationalUnitID) {
+        super(username, password, organisationalUnitID);
+
+    }
+}
