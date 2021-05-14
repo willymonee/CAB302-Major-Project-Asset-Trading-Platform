5d16bbbe84d2505a33eda371f5f1a213ea01f43a
dandragoneel <danieljong3000@hotmail.com>
2021-05-14 12:39:48 +1000

    added buy/sell offer in marketplacedatasource

--- src/ElectronicAssetTradingPlatform/Database/MarketplaceDataSource.java
+++ src/ElectronicAssetTradingPlatform/Database/MarketplaceDataSource.java
@@ -8,6 +8,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class MarketplaceDataSource {
     private static final String INSERT_BUYOFFER = "INSERT INTO Marketplace (Trade_ID, Buy_or_Sell, "
@@ -69,6 +72,82 @@ public class MarketplaceDataSource {
             e.printStackTrace();
         }
     }
+
+    public void insertSellOffer(User user, Asset asset, String assetPrice) {
+        try {
+            insertBuyOffer.setString(1, "null");
+            insertBuyOffer.setString(2, "s");
+            // Get Unit ID
+            // switch case for org unit mem, leader and whoever else can create a buy offer
+            if (user.getClass() == OrganisationalUnitMembers.class) {
+                UnitDataSource unitDB = new UnitDataSource();
+                String unitID = unitDB.executeGetUnitID(((OrganisationalUnitMembers) user).getUnitName());
+                insertBuyOffer.setString(3, unitID);
+            }
+
+            else {
+                //
+            }
+            if (user.getClass() == OrganisationalUnitMembers.class) {
+                UnitDataSource unitDB = new UnitDataSource();
+                String userID = unitDB.executeGetUserID(((OrganisationalUnitMembers) user).getUnitName());
+                insertBuyOffer.setString(4, userID);
+            }
+            insertBuyOffer.setString(5, asset.getAssetName());
+            insertBuyOffer.setString(6, assetPrice);
+            insertBuyOffer.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // maybe a param can be unit credits BUY/SELL get offers can be merged into 1 method
+    public HashMap<String, String> getBuyOffers(User user) {
+        HashMap<String, String> buyOffers = new HashMap<String, String>();
+        ResultSet rs = null;
+
+        try {
+            //getBuyOffers.setString(1, "b");
+            rs = getBuyOffers.executeQuery();
+            while(rs.next()) {
+                buyOffers.put(rs.getString("Asset_type_ID"), rs.getString("Price_per_unit"));
+            }
+            rs.close();
+            // maybe stuff about querying user
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return buyOffers;
+    }
+
+    // maybe a param can be unit credits
+    public HashMap<String, String> getSellOffers(User user) {
+        HashMap<String, String> sellOffers = new HashMap<String, String>();
+        ResultSet rs = null;
+
+        try {
+            rs = getBuyOffers.executeQuery();
+            while(rs.next()) {
+                sellOffers.put(rs.getString("Asset_type_ID"), rs.getString("Price_per_unit"));
+            }
+            rs.close();
+            // maybe stuff about querying user
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return sellOffers;
+    }
+
+    /* maybe params r unit id, user id, ppu
+    public void resolveOffer(User user, Asset asset) {
+        try {
+            // get the offer id from marketplace
+            // resolveOffer.setString("1", )
+        }
+    }
+    */
+
 
 
 }
